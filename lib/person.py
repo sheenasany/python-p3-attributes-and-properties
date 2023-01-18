c876423a64c4e6dca4ffb6ed9748fbf2523c7e40
Sheena Sang <sheenasany@gmail.com>
2023-01-17 23:37:25 -0500

    completed ridunkulous lab

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,27 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def get_name(self):
+        return self._name
+    
+    def set_name(self, name):
+        if type(name) == str and 1 <= len(name) <= 25:
+            self._name = name.title()
+        
+        else:
+            print('Name must be string between 1 and 25 characters.')
+    
+    name = property(get_name, set_name)
+    
+    def get_job(self):
+        return self._job
+    
+    def set_job(self, job):
+        approved_jobs = ["Admin", "Customer Service", "Human Resources", "ITC", "Production", "Legal", "Finance", "Sales", "General Management", "Research & Development", "Marketing", "Purchasing"]
+        if job in approved_jobs:
+            self._job = job
+        
+        else:
+            print(f"Job must be in list of approved jobs.")
+    
+    job = property(get_job, set_job)
